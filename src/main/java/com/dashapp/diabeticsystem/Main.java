334ab405f3e6258ce61614ebd07923b989291fa3
mattiadane <mattia.danese04@gmail.com>
2025-05-06 15:00:14 +0200

    login

--- src/main/java/com/dashapp/diabeticsystem/Main.java
+++ src/main/java/com/dashapp/diabeticsystem/Main.java
@@ -1,5 +1,6 @@
 package com.dashapp.diabeticsystem;
 
+import com.dashapp.diabeticsystem.models.DbManager;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
@@ -9,6 +10,8 @@ import java.io.IOException;
 import java.net.URL;
 
 public class Main extends Application {
+    private static final DbManager dbManager = new DbManager();
+
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("fxml/login.fxml"));
@@ -27,5 +30,9 @@ public class Main extends Application {
 
     public static void main(String[] args) {
         launch();
+    }
+
+    public static DbManager getDbManager() {
+        return dbManager;
     }
 }
---
+++ src/main/java/com/dashapp/diabeticsystem/models/Login.java
@@ -0,0 +1,23 @@
+package com.dashapp.diabeticsystem.models;
+
+import com.dashapp.diabeticsystem.Main;
+
+import java.sql.ResultSet;
+
+public class Login {
+
+
+
+    public User getUser(String username, String password) {
+         return  Main.getDbManager().selectQuery("Select * FROM login WHERE username  = ? AND password_hash = ?",
+                 rs -> {
+                    if(rs.next()) {
+                        return new User(rs.getString("username"));
+                    }
+                     return null;
+                 }
+                 ,username,password);
+
+    }
+
+}
