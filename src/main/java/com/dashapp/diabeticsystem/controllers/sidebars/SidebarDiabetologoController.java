0b3cf71ff5ac067dd4b97b784ac20f12f0c8f047
mattiadane <mattia.danese04@gmail.com>
2025-05-09 11:26:37 +0200

    sistemati file fxml per le sidebar

--- src/main/java/com/dashapp/diabeticsystem/controllers/sidebars/SidebarDiabetologoController.java
+++ src/main/java/com/dashapp/diabeticsystem/controllers/sidebars/SidebarDiabetologoController.java
@@ -1,7 +1,13 @@
 package com.dashapp.diabeticsystem.controllers.sidebars;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
 public class SidebarDiabetologoController {
 
-
+    @FXML private Button homeButtonD;
+    @FXML private Button pazientiButtonD;
+    @FXML private Button settingsButtonD;
+    @FXML private Button logoutButton;
 
 }
--- src/main/java/com/dashapp/diabeticsystem/controllers/sidebars/SidebarPazienteController.java
+++ src/main/java/com/dashapp/diabeticsystem/controllers/sidebars/SidebarPazienteController.java
@@ -1,4 +1,12 @@
 package com.dashapp.diabeticsystem.controllers.sidebars;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
 public class SidebarPazienteController {
+
+    @FXML private Button homeButtonP;
+    @FXML private Button terapieButtonP;
+    @FXML private Button settingsButtonP;
+    @FXML private Button logoutButton;
 }
